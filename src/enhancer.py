a583ecadbeef56aa5d25c33af6ce6d6661980c55
Parth Naik <parthnaik1993@gmail.com>
2026-01-09 17:50:19 +0100

    Remove unused fuse_to_enhance method
    Removed commented-out code for HSI enhancement.

--- src/enhancer.py
+++ src/enhancer.py
@@ -43,23 +43,3 @@ class HSIEnhancer:
             hsi_upsampled[..., band] = hsi_hr_band
     
         return hsi_upsampled
-    
-    # def fuse_to_enhance(self, patch_size=12, stride=1, guide_radius=1, detail_weight=3.5):
-    #     """Perform HSI enhancement by fusing with MSI."""
-    #     patch_processor = PatchProcessor(self.hsi, self.msi, self.n_components, self.n_atoms, self.lambda_reg)
-    #     hsi_hr = patch_processor.run_parallel(patch_size, stride)
-    #     selected_bands = [1, 7, 11]
-    #     msi_guide = self.msi[..., selected_bands].astype(np.float32)
-
-    #     hsi_upsampled = self.upsampler.enhanced_hsi_upsampling(self.hsi, self.msi, msi_guide, detail_weight)
-
-    #     for band in range(hsi_upsampled.shape[-1]):
-    #         hsi_hr_band = cv2.ximgproc.guidedFilter(
-    #             guide=msi_guide,
-    #             src=hsi_upsampled[..., band] + hsi_hr[..., band],
-    #             radius=guide_radius,
-    #             eps=0.0001
-    #         )
-    #         hsi_upsampled[..., band] = hsi_hr_band
-
-    #     return hsi_upsampled
